ac22fed8a34e95dddb77a91f280848efbe030e5e
ellkoell <ellkoell@tsn.at>
2025-10-17 10:05:30 +0200

    dateistruktur aufgebaut, bootstrap inkludiert

--- lib/index.php
+++ lib/index.php
@@ -1,5 +1,20 @@
+<!doctype html>
+<html lang="de">
+<head>
+    <!--Client und Serverseitige Validierung, clientseitige deshalb, weil schon vor abschicken geprüft wird, und dann
+      bessere usability-->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Benutzerdaten</title>
 
 
+</head>
+
+<h1>Benutzerdaten anzeigen</h1>
+<body>
 
 
+</body>
 <?php
